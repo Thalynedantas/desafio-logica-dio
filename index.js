56e9cbc97a8ec8292bfd6d13ace2b5906bbcb3f4
Thalyne Dantas <thalyne13@gmail.com>
2024-01-11 19:43:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let nomeHeroi = "Batman"
+let xpHeroi = 6721
+
+if (xpHeroi <= 1000) {
+    console.log( "O Herói de nome " + nomeHeroi + " está no nível Ferro")
+} else if (xpHeroi <= 2000) {
+    console.log( "O Herói de nome " + nomeHeroi + " está no nível Bronze")
+} else if (xpHeroi <= 5000) {
+    console.log( "O Herói de nome " + nomeHeroi + " está no nível Prata")
+} else if (xpHeroi <= 7000) {
+    console.log( "O Herói de nome " + nomeHeroi + " está no nível Ouro")
+} else if (xpHeroi <= 8000) {
+    console.log( "O Herói de nome " + nomeHeroi + " está no nível Platina Diamante")
+} else if (xpHeroi <= 9000) {
+    console.log( "O Herói de nome " + nomeHeroi + " está no nível Ascendente")
+} else if (xpHeroi <= 10000) {
+    console.log( "O Herói de nome " + nomeHeroi + " está no nível Imortal")
+} else if (xpHeroi >= 10001) {
+    console.log( "O Herói de nome " + nomeHeroi + " está no nível Radiante");
+}
